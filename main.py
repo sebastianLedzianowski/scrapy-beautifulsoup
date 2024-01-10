5de39f14eb2c49a8fe7d84656cd024a2438dbcd8
sebastianLedzianowski <136706736+sebastianLedzianowski@users.noreply.github.com>
2024-01-10 17:02:26 +0100

    Reading files and writing them to the mongoDB database has been added to the main.py file.

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+import logging
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from mongodb.connect import connect_mongodb
+from mongodb.models import Quote, Author
+from mongodb.tools_file_and_mongodb import open_file, save_to_database
 
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s]: %(message)s')
+    connect_mongodb()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    data_base_spyder_quote = open_file('scrapy_spyder/scrapy_spyder/spiders/quotes_spyder.json')
+    for data in data_base_spyder_quote:
+        save_to_database(Quote, data)
 
+    data_base_spyder_author = open_file('scrapy_spyder/scrapy_spyder/spiders/authors_spyder.json')
+    for data in data_base_spyder_author:
+        save_to_database(Author, data)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    data_base_soup_quote = open_file('beautifulsoup/quotes_soup.json')
+    for data in data_base_soup_quote:
+        save_to_database(Quote, data)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    data_base_soup_author = open_file('beautifulsoup/authors_soup.json')
+    for data in data_base_soup_author:
+        save_to_database(Author, data)
